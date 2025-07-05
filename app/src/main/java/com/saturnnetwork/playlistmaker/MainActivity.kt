94f7916939bff248dcf8b9ef6114ff7d67a22218
JosephPtn <145558577+JosephPtn@users.noreply.github.com>
2025-07-05 19:50:05 +0300

    second commit 05.07 19:48

--- app/src/main/java/com/saturnnetwork/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/saturnnetwork/playlistmaker/MainActivity.kt
@@ -13,26 +13,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
 
         val findBtn = findViewById<Button>(R.id.findBtn)
         val mediaLibrBtn = findViewById<Button>(R.id.mediaLibrBtn)
         val settingsBtn = findViewById<Button>(R.id.settingsBtn)
 
-        val findBtnOnClickListener: View.OnClickListener = object : View.OnClickListener { override fun onClick(v: View?) {
+        findBtn.setOnClickListener {
+            val intentToSearchActivity = Intent(
+                this,
+                SearchActivity::class.java
+            )
+            this.startActivity(intentToSearchActivity)
 
-            val context = v?.context
-            val intentToSearchActivity = Intent(context, SearchActivity::class.java)
-            context?.startActivity(intentToSearchActivity)
-            }
         }
-        findBtn.setOnClickListener(findBtnOnClickListener)
 
         mediaLibrBtn.setOnClickListener {
             val intentToMediaLibrariesActivity = Intent(
--- app/src/main/java/com/saturnnetwork/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/saturnnetwork/playlistmaker/SettingsActivity.kt
@@ -11,13 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
 
         val backBtn = findViewById<Button>(R.id.backBtn)
         backBtn.setOnClickListener {
